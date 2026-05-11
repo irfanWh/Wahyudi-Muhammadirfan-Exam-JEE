95c0d946368404197694f07f4783fe5bf87c4194
irfanWh <muhammadiw456@gmail.com>
2026-05-11 08:57:50 +0100

    backend

---
+++ controle-backend/src/main/java/ma/enset/controlebackend/ControleBackendApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.controlebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControleBackendApplication.class, args);
+    }
+
+}
---
+++ controle-backend/src/test/java/ma/enset/controlebackend/ControleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.controlebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
